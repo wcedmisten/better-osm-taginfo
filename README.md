d1b007e36e5eff8de730156bd4d77ece34cc43b9
William Edmisten <wcedmisten@gmail.com>
2024-12-30 20:39:34 -0500

    Scale height on grid

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # better-osm-taginfo
 Userscript adding UI improvments for taginfo.openstreetmap.org
 
+Currently this is limited to adding a responsive design to the `/keys` page, but this may expand to other areas in the future.
+
+Let me know if you have any suggestions on things to improve, I'm open to discussion!
+
 1. Install [Tampermonkey](https://www.tampermonkey.net) or [Violentmonkey](https://violentmonkey.github.io/)
 2. [Install](https://github.com/wcedmisten/better-osm-taginfo/raw/refs/heads/main/better-osm-taginfo.user.js) script
+
+## Current Limitations
+
+* The `Distribution of values` section is a fixed width and cannot be widened to match the available space
+* The height of the Chronology graph is still not responsive
+* The image under `Wiki` is usually only 300px, which does not utilize the full width on desktop
--- better-osm-taginfo.user.js
+++ better-osm-taginfo.user.js
@@ -12,24 +12,48 @@ const isDesktop = window.matchMedia('(width >= 768px)');
 
 const applyGridLayout = () => {
   document.getElementsByClassName("overview-container")[0].style.display = "grid";
+  document.getElementsByClassName("overview-container")[0].style.height = "90%";
   document.getElementsByClassName("overview-container")[0].style["grid-template-columns"] = "1fr 1fr";
 
   Array.from(document.getElementsByClassName("overview-item")).forEach((i) => {
     i.style.width = "unset";
+    i.style.height = "90%";
   });
+
+
+  document.getElementsByClassName("overview-item-wiki")[0].style["align-items"] = "center";
+
+  const languagesDiv = document.querySelector("div.overview-item:nth-child(4) > div:nth-child(3)");
+  languagesDiv.style["max-width"] = "500px"
+
+
+  document.getElementsByClassName("overview-item-wiki-image")[0].style.width = "100%";
 }
 
+// NOTE: for every CSS prop we apply on the new grid layout, we need to unset it on the flex layout to restore the original CSS
 const applyFlexLayout = () => {
   document.getElementsByClassName("overview-container")[0].style.display = "flex";
+  document.getElementsByClassName("overview-container")[0].style.height = "unset";
   document.getElementsByClassName("overview-container")[0].style["grid-template-columns"] = "unset";
 
   Array.from(document.getElementsByClassName("overview-item")).forEach((i) => {
     i.style.width = "100%";
+    i.style.height = "unset";
   });
+
+  document.getElementsByClassName("overview-item-wiki")[0].style["align-items"] = "unset";
+
+  const languagesDiv = document.querySelector("div.overview-item:nth-child(4) > div:nth-child(3)");
+  languagesDiv.style["max-width"] = "unset"
+
+
+  document.getElementsByClassName("overview-item-wiki-image")[0].style.width = "unset";
 }
 
 if (isDesktop.matches) {
   applyGridLayout();
+} else {
+  applyFlexLayout();
 }
 
 
