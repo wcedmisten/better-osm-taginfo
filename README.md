bba9436857cc7f6a8a80a91a45b21e20bfdfb3bd
William Edmisten <wcedmisten@gmail.com>
2024-12-30 19:31:02 -0500

    Commit userscript

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # better-osm-taginfo
 Userscript adding UI improvments for taginfo.openstreetmap.org
+
+1. Install [Tampermonkey](https://www.tampermonkey.net) or [Violentmonkey](https://violentmonkey.github.io/)
+2. [Install](https://raw.githubusercontent.com/wcedmisten/better-osm-taginfo/better-osm-taginfo.user.js) script
---
+++ better-osm-taginfo.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        Better Taginfo
+// @namespace   https://github.com/wcedmisten/better-osm-taginfo
+// @match       https://taginfo.openstreetmap.org/keys/*
+// @grant       none
+// @version     0.1
+// @author      wcedmisten
+// @description Userscript adding UI improvments for taginfo.openstreetmap.org
+// ==/UserScript==
+
+const isDesktop = window.matchMedia('(width >= 768px)');
+
+const applyGridLayout = () => {
+  document.getElementsByClassName("overview-container")[0].style.display = "grid";
+  document.getElementsByClassName("overview-container")[0].style["grid-template-columns"] = "1fr 1fr";
+
+  Array.from(document.getElementsByClassName("overview-item")).forEach((i) => {
+    i.style.width = "unset";
+  });
+}
+
+const applyFlexLayout = () => {
+  document.getElementsByClassName("overview-container")[0].style.display = "flex";
+  document.getElementsByClassName("overview-container")[0].style["grid-template-columns"] = "unset";
+
+  Array.from(document.getElementsByClassName("overview-item")).forEach((i) => {
+    i.style.width = "100%";
+  });
+}
+
+if (isDesktop.match) {
+  applyGridLayout();
+}
+
+
+isDesktop.onchange = (e) => {
+  if (e.matches) {
+    applyGridLayout();
+  } else {
+    applyFlexLayout();
+  }
+};
